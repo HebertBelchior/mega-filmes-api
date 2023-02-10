7eb93e93157f2f6e2d900e5574017bf788582de2
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-02-10 11:44:34 -0300

    feat: configura o appdbcontext

---
+++ MegaFilmes.Api/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using MegaFilmes.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MegaFilmes.Api.Data;
+
+public class AppDbContext : DbContext
+{
+	public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+	public DbSet<Filme> Filmes { get; set; }
+	public DbSet<Avaliacao> Avaliacoes { get; set; }
+	public DbSet<Ator> Atores { get; set; }
+	public DbSet<FilmesAtores> FilmesAtores { get; set; }
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
