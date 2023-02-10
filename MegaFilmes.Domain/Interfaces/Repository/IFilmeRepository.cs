e19096815342cb832ee74438efa22682ef7cfec2
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-02-10 16:03:49 -0300

    feat(filme): cria o repositório para filme

---
+++ MegaFilmes.Domain/Interfaces/Repository/IFilmeRepository.cs
@@ -0,0 +1,15 @@
+﻿using MegaFilmes.Domain.Entities;
+
+namespace MegaFilmes.Domain.Interfaces.Repository;
+
+public interface IFilmeRepository
+{
+    Task<ICollection<Filme>> GetAllAsync();
+    Task<Filme?> GetByIdAsync(int id);
+    Task<Filme> CreateAsync(Filme filme);
+    Task<Filme> UpdateAsync(Filme filme);
+    Task DeleteAsync(Filme filme);
+    Task<Filme?> GetByGender(string genero);
+    Task<IEnumerable<Filme?>> GetByDirector(string diretor);
+    Task<IEnumerable<Filme?>> GetByName(string nome);
+}
---
+++ MegaFilmes.Infra/Repository/FilmeRepository.cs
@@ -0,0 +1,65 @@
+﻿using MegaFilmes.Domain.Entities;
+using MegaFilmes.Domain.Interfaces.Repository;
+using MegaFilmes.Infra.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace MegaFilmes.Infra.Repository;
+
+public class FilmeRepository : IFilmeRepository
+{
+    private readonly AppDbContext _db;
+
+    public FilmeRepository(AppDbContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<Filme> CreateAsync(Filme filme)
+    {
+        _db.Add(filme);
+        await _db.SaveChangesAsync();
+        return filme;
+    }
+
+    public async Task DeleteAsync(Filme filme)
+    {
+        _db.Remove(filme);
+        await _db.SaveChangesAsync();
+    }
+
+    public async Task<ICollection<Filme>> GetAllAsync()
+    {
+        return await _db.Filmes.ToListAsync();
+    }
+
+    public async Task<IEnumerable<Filme?>> GetByDirector(string diretor)
+    {
+        return await _db.Filmes
+            .Where(x => EF.Functions.Like(x.Diretor, $"%{diretor}%"))
+            .ToListAsync();
+    }
+
+    public async Task<Filme?> GetByGender(string genero)
+    {
+        return await _db.Filmes.FirstOrDefaultAsync(x => x.Genero == genero);
+    }
+
+    public async Task<Filme?> GetByIdAsync(int id)
+    {
+        return await _db.Filmes.FirstOrDefaultAsync(x => x.Id == id);
+    }
+
+    public async Task<IEnumerable<Filme?>> GetByName(string nome)
+    {
+        return await _db.Filmes
+            .Where(x => EF.Functions.Like(x.Nome, $"%{nome}%"))
+            .ToListAsync();
+    }
+
+    public async Task<Filme> UpdateAsync(Filme filme)
+    {
+        _db.Update(filme);
+        await _db.SaveChangesAsync();
+        return filme;
+    }
+}
