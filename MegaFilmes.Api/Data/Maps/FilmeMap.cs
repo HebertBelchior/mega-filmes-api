bb60e57ae00eeebed2ab55228fb188eafcd952f8
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-02-10 11:45:11 -0300

    feat: cria configuracao de tabela da entidade filme

---
+++ MegaFilmes.Api/Data/Maps/FilmeMap.cs
@@ -0,0 +1,41 @@
+﻿using MegaFilmes.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MegaFilmes.Api.Data.Maps;
+
+public class FilmeMap : IEntityTypeConfiguration<Filme>
+{
+    public void Configure(EntityTypeBuilder<Filme> builder)
+    {
+        builder.ToTable("filmes");
+
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id)
+            .UseIdentityColumn()
+            .HasColumnName("id")
+            .IsRequired();
+
+        builder.Property(x => x.Nome)
+            .HasColumnName("nome")
+            .IsRequired();
+
+        builder.Property(x => x.Descricao)
+            .HasColumnName("descricao")
+            .IsRequired();
+
+        builder.Property(x => x.Ano)
+            .HasColumnName("ano")
+            .IsRequired();
+
+        builder.Property(x => x.Diretor)
+            .HasColumnName("diretor")
+            .IsRequired();
+
+        builder.Property(x => x.Genero)
+            .HasColumnName("genero")
+            .IsRequired();
+    }
+
+
+}
