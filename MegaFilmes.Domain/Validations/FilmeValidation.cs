9d6a741ac60b2782dbbfa928f997f44b9b2583e5
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-02-10 22:00:24 -0300

    feat(filmes): cria a validação com o fluentapi

---
+++ MegaFilmes.Domain/Validations/FilmeValidation.cs
@@ -0,0 +1,56 @@
+﻿using FluentValidation;
+using MegaFilmes.Domain.Dtos.FilmeDto;
+
+namespace MegaFilmes.Domain.Validations;
+
+public class CreateFilmeValidation : AbstractValidator<CreateFilmeDto>
+{
+	public CreateFilmeValidation()
+	{
+		RuleFor(x => x.Nome)
+			.NotEmpty()
+			.NotNull();
+
+        RuleFor(x => x.Descricao)
+            .NotEmpty()
+            .NotNull();
+
+        RuleFor(x => x.Ano)
+            .NotEmpty()
+            .NotNull();
+
+        RuleFor(x => x.Diretor)
+            .NotEmpty()
+            .NotNull();
+
+        RuleFor(x => x.Genero)
+            .NotEmpty()
+            .NotNull();
+    }
+}
+
+public class UpdateFilmeValidation : AbstractValidator<UpdateFilmeDto>
+{
+    public UpdateFilmeValidation()
+    {
+        RuleFor(x => x.Nome)
+            .NotEmpty()
+            .NotNull();
+
+        RuleFor(x => x.Descricao)
+            .NotEmpty()
+            .NotNull();
+
+        RuleFor(x => x.Ano)
+            .NotEmpty()
+            .NotNull();
+
+        RuleFor(x => x.Diretor)
+            .NotEmpty()
+            .NotNull();
+
+        RuleFor(x => x.Genero)
+            .NotEmpty()
+            .NotNull();
+    }
+}
