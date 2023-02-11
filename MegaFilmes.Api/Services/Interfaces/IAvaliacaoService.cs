0310db5dcbe421d8569d12579417d7b068a7a96b
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-02-11 14:01:51 -0300

    feat(avaliacao): cria um serviço para avaliacao

---
+++ MegaFilmes.Api/Services/Interfaces/IAvaliacaoService.cs
@@ -0,0 +1,8 @@
+﻿using MegaFilmes.Domain.Dtos.AvaliacaoDto;
+
+namespace MegaFilmes.Api.Services.Interfaces;
+
+public interface IAvaliacaoService
+{
+    Task<ResultService<ReadAvaliacaoDto>> CreateAsync(CreateAvaliacaoDto avaliacaoDto);
+}
