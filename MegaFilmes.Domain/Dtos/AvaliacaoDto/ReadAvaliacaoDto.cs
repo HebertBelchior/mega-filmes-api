81e75bbcc73974f0b8dd817d638bd0f4e66e96aa
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-02-11 16:28:58 -0300

    refactor(avaliacao): altera o readdto

--- MegaFilmes.Domain/Dtos/AvaliacaoDto/ReadAvaliacaoDto.cs
+++ MegaFilmes.Domain/Dtos/AvaliacaoDto/ReadAvaliacaoDto.cs
@@ -1,7 +1,9 @@
 ﻿namespace MegaFilmes.Domain.Dtos.AvaliacaoDto;
 
-public class ReadAvaliacaoDto : CreateAvaliacaoDto
+public class ReadAvaliacaoDto
 {
     public int Id { get; set; }
-
+    public int Criterio { get; set; }
+    public string Comentario { get; set; }
+    public string Filme { get; set; }
 }
