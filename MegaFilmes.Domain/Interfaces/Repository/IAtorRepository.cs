ced0b6ddb86d1e808be489d0564f076e07e67acb
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-02-11 14:05:13 -0300

    style: converte para escopo de arquivo

--- MegaFilmes.Domain/Interfaces/Repository/IAtorRepository.cs
+++ MegaFilmes.Domain/Interfaces/Repository/IAtorRepository.cs
@@ -1,19 +1,13 @@
 ﻿using MegaFilmes.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace MegaFilmes.Domain.Interfaces.Repository
+namespace MegaFilmes.Domain.Interfaces.Repository;
+
+public interface IAtorRepository
 {
-    public interface IAtorRepository
-    {
-        Task<ICollection<Ator>> GetAllAsync();
-        Task<Ator?> GetByIdAsync(int id);
-        Task<Ator> CreateAsync(Ator ator);
-        Task<Ator> UpdateAsync(Ator ator);
-        Task DeleteAsync(Ator ator);
-        Task<IEnumerable<Ator?>> GetByName(string nome);
-    }
+    Task<ICollection<Ator>> GetAllAsync();
+    Task<Ator?> GetByIdAsync(int id);
+    Task<Ator> CreateAsync(Ator ator);
+    Task<Ator> UpdateAsync(Ator ator);
+    Task DeleteAsync(Ator ator);
+    Task<IEnumerable<Ator?>> GetByName(string nome);
 }
--- MegaFilmes.Infra/Repository/AvaliacaoRepository.cs
+++ MegaFilmes.Infra/Repository/AvaliacaoRepository.cs
@@ -3,45 +3,44 @@ using MegaFilmes.Domain.Interfaces.Repository;
 using MegaFilmes.Infra.Context;
 using Microsoft.EntityFrameworkCore;
 
-namespace MegaFilmes.Infra.Repository
+namespace MegaFilmes.Infra.Repository;
+
+public class AvaliacaoRepository : IAvaliacaoRepository
 {
-    public class AvaliacaoRepository : IAvaliacaoRepository
+    private readonly AppDbContext _db;
+
+    public AvaliacaoRepository(AppDbContext db)
     {
-        private readonly AppDbContext _db;
+        _db = db;
+    }
 
-        public AvaliacaoRepository(AppDbContext db)
-        {
-            _db = db;
-        }
+    public async Task<Avaliacao> CreateAsync(Avaliacao avaliacao)
+    {
+        _db.Add(avaliacao);
+        await _db.SaveChangesAsync();
+        return avaliacao;
+    }
 
-        public async Task<Avaliacao> CreateAsync(Avaliacao avaliacao)
-        {
-            _db.Add(avaliacao);
-            await _db.SaveChangesAsync();
-            return avaliacao;
-        }
+    public async Task DeleteAsync(Avaliacao avaliacao)
+    {
+        _db.Remove(avaliacao);
+        await _db.SaveChangesAsync();
+    }
 
-        public async Task DeleteAsync(Avaliacao avaliacao)
-        {
-            _db.Remove(avaliacao);
-            await _db.SaveChangesAsync();
-        }
+    public async Task<ICollection<Avaliacao>> GetAllAsync()
+    {
+        return await _db.Avaliacoes.ToListAsync();
+    }
 
-        public async Task<ICollection<Avaliacao>> GetAllAsync()
-        {
-            return await _db.Avaliacoes.ToListAsync();
-        }
+    public async Task<Avaliacao?> GetByIdAsync(int id)
+    {
+        return await _db.Avaliacoes.FirstOrDefaultAsync(x => x.Id == id);
+    }
 
-        public async Task<Avaliacao?> GetByIdAsync(int id)
-        {
-            return await _db.Avaliacoes.FirstOrDefaultAsync(x => x.Id == id);
-        }
-
-        public async Task<Avaliacao> UpadateAsync(Avaliacao avaliacao)
-        {
-            _db.Update(avaliacao);
-            await _db.SaveChangesAsync();
-            return avaliacao;
-        }
+    public async Task<Avaliacao> UpadateAsync(Avaliacao avaliacao)
+    {
+        _db.Update(avaliacao);
+        await _db.SaveChangesAsync();
+        return avaliacao;
     }
 }
