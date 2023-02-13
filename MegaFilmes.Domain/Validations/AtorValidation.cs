69f1400e30b03816a0183c7b88f741c568abec87
Hebert <hebert.moura@hotmail.com>
2023-02-13 18:16:34 -0300

    cria validações para Ator e FilmeAtor

---
+++ MegaFilmes.Domain/Validations/AtorValidation.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using MegaFilmes.Domain.Dtos.AtorDto;
+
+namespace MegaFilmes.Domain.Validations;
+
+public class CreateAtorValidation : AbstractValidator<CreateAtorDto>
+{
+    public CreateAtorValidation()
+    {
+        RuleFor(x => x.Nome)
+            .NotEmpty()
+            .NotNull();
+    }
+}
+
+public class UpdateAtorValidation : AbstractValidator<UpdateAtorDto>
+{
+    public UpdateAtorValidation()
+    {
+        RuleFor(x => x.Nome)
+            .NotEmpty()
+            .NotNull();
+    }
+}
