aa8f669d30470db49b8093db46609738f801bb9b
Hebert <hebert.moura@hotmail.com>
2023-02-13 18:16:02 -0300

    cria Dto para Ator e para FilmeAtor

---
+++ MegaFilmes.Domain/Dtos/FilmeAtorDto/ReadFilmeAtorDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MegaFilmes.Domain.Dtos.FilmeAtorDto;
+
+public class ReadFilmeAtorDto
+{
+    public int Id { get; set; }
+    public int FilmeId { get; set; }
+    public int AtorId { get; set; }
+    public string Papel { get; set; }
+}
