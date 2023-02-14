7fe27a21ab0523b89cefd02e404c3e57d596a28f
Hebert <hebert.moura@hotmail.com>
2023-02-14 15:28:05 -0300

    cria interface do serviço

---
+++ MegaFilmes.Api/Services/Interfaces/IFilmeAtorService.cs
@@ -0,0 +1,15 @@
+﻿using MegaFilmes.Domain.Dtos.FilmeAtorDto;
+
+namespace MegaFilmes.Api.Services.Interfaces;
+
+public interface IFilmeAtorService
+{
+    Task<ResultService<ICollection<ReadFilmeAtorDto>>> GetAllAsync();
+    Task<ResultService<ReadFilmeAtorDto>> GetByIdAsync(int id);
+    Task<ResultService<ReadFilmeAtorDto>> CreateAsync(CreateFilmeAtorDto filmeAtorDto);
+    Task<ResultService<ReadFilmeAtorDto>> UpdateAsync(int id, UpdateFilmeAtorDto filmeAtorDto);
+    Task<ResultService> DeleteAsync(int id);
+    Task<ResultService<ICollection<ReadFilmeAtorDto>>> GetByFilmeId(int id);
+    Task<ResultService<ICollection<ReadFilmeAtorDto>>> GetByAtorId(int id);
+    Task<ResultService<ICollection<ReadFilmeAtorDto>>> GetByPapel(string papel);
+}
