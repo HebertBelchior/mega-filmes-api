deee8dea331329657bfda4dc92d39aca008f0398
Hebert <hebert.moura@hotmail.com>
2023-02-14 15:24:49 -0300

    faz modificações necessárias no IAtorRepository e IFilmeAtorRepository

--- MegaFilmes.Domain/Interfaces/Repository/IFilmeAtorRepository.cs
+++ MegaFilmes.Domain/Interfaces/Repository/IFilmeAtorRepository.cs
@@ -9,4 +9,8 @@ public interface IFilmeAtorRepository
     Task<FilmeAtor> CreateAsync(FilmeAtor filmeAtor);
     Task<FilmeAtor> UpdateAsync(FilmeAtor filmeAtor);
     Task DeleteAsync(FilmeAtor filmeAtor);
+    Task<FilmeAtor?> GetByFilmeId(int id);
+    Task<FilmeAtor?> GetByAtorId(int id);
+    Task<ICollection<FilmeAtor>> GetByPapel(string papel);
+    Task<FilmeAtor?> CheckExists(int atorId, int filmeId, string papel);
 }
