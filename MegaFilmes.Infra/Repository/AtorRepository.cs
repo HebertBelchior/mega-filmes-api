845cf983627b3e34581267dd31956ad924bcbcc9
Hebert <hebert.moura@hotmail.com>
2023-02-14 15:22:16 -0300

    faz modificações necessárias no AtorRepository e FilmeAtorRepository

--- MegaFilmes.Infra/Repository/AtorRepository.cs
+++ MegaFilmes.Infra/Repository/AtorRepository.cs
@@ -14,6 +14,11 @@ public class AtorRepository : IAtorRepository
         _db = db;
     }
 
+    public async Task<Ator?> CheckAtorExists(string nome)
+    {
+        return await _db.Atores.FirstOrDefaultAsync(x => x.Nome.ToLower() == nome.ToLower());
+    }
+
     public async Task<Ator> CreateAsync(Ator ator)
     {
         _db.Add(ator);
@@ -29,19 +34,16 @@ public class AtorRepository : IAtorRepository
 
     public async Task<ICollection<Ator>> GetAllAsync()
     {
-        _db.Filmes.Include(f => f.FilmesAtores).ThenInclude(fa => fa.Ator).ToList();
         return await _db.Atores.ToListAsync();
     }
 
     public async Task<Ator?> GetByIdAsync(int id)
     {
-        _db.Filmes.Include(f => f.FilmesAtores).ThenInclude(fa => fa.Ator).FirstOrDefault(f => f.Id == id);
-        return await _db.Atores.FirstOrDefaultAsync(x => x.Id == id);
+        return await _db.Atores.Include(a => a.FilmesAtores).ThenInclude(fa => fa.Filme).FirstOrDefaultAsync(a => a.Id == id);
     }
 
-    public async Task<IEnumerable<Ator?>> GetByName(string nome)
+    public async Task<IEnumerable<Ator>> GetByName(string nome)
     {
-        _db.Filmes.Include(f => f.FilmesAtores).ThenInclude(fa => fa.Ator).FirstOrDefault(f => f.Nome == nome);
         return await _db.Atores
         .Where(x => EF.Functions.Like(x.Nome, $"%{nome}%"))
         .ToListAsync();
--- MegaFilmes.Infra/Repository/FilmeAtorRepository.cs
+++ MegaFilmes.Infra/Repository/FilmeAtorRepository.cs
@@ -1,5 +1,4 @@
-﻿
-using MegaFilmes.Domain.Entities;
+﻿using MegaFilmes.Domain.Entities;
 using MegaFilmes.Domain.Interfaces.Repository;
 using MegaFilmes.Infra.Context;
 using Microsoft.EntityFrameworkCore;
@@ -13,6 +12,11 @@ public class FilmeAtorRepository : IFilmeAtorRepository
     public FilmeAtorRepository(AppDbContext db)
     {
         _db = db;
+    }
+
+    public async Task<FilmeAtor?> CheckExists(int atorId, int filmeId, string papel)
+    {
+        return await _db.FilmesAtores.FirstOrDefaultAsync(x => x.AtorId == atorId && x.FilmeId == filmeId && x.Papel == papel);
     }
 
     public async Task<FilmeAtor> CreateAsync(FilmeAtor filmeAtor)
@@ -30,14 +34,31 @@ public class FilmeAtorRepository : IFilmeAtorRepository
 
     public async Task<ICollection<FilmeAtor>> GetAllAsync()
     {
-        _db.FilmesAtores.Include(f => f.Filme).Include(fa => fa.Ator).ToList();
+        await _db.FilmesAtores.Include(f => f.Filme).Include(fa => fa.Ator).ToListAsync();
         return await _db.FilmesAtores.ToListAsync();
+    }
+
+    public async Task<FilmeAtor?> GetByAtorId(int id)
+    {
+        await _db.FilmesAtores.Include(f => f.Filme).Include(fa => fa.Ator).FirstOrDefaultAsync(f => f.Id == id);
+        return await _db.FilmesAtores.FirstOrDefaultAsync(x => x.Id == id);
+    }
+
+    public async Task<FilmeAtor?> GetByFilmeId(int id)
+    {
+        await _db.FilmesAtores.Include(f => f.Filme).Include(fa => fa.Ator).FirstOrDefaultAsync(f => f.Id == id);
+        return await _db.FilmesAtores.FirstOrDefaultAsync(x => x.Id == id);
     }
 
     public async Task<FilmeAtor?> GetByIdAsync(int id)
     {
-        _db.FilmesAtores.Include(f => f.Ator).Include(fa => fa.Filme).FirstOrDefault(f => f.Id == id);
+        await _db.FilmesAtores.Include(f => f.Filme).Include(fa => fa.Ator).FirstOrDefaultAsync(f => f.Id == id);
         return await _db.FilmesAtores.FirstOrDefaultAsync(x => x.Id == id);
+    }
+
+    public async Task<ICollection<FilmeAtor>> GetByPapel(string papel)
+    {
+        return await _db.FilmesAtores.Include(f => f.Filme).Include(fa => fa.Ator).Where(x => x.Papel.Contains(papel)).ToListAsync();
     }
 
     public async Task<FilmeAtor> UpdateAsync(FilmeAtor filmeAtor)
