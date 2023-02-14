0036e602c7c1c78e62902b4ae72c5062f8fcc41b
Hebert <hebert.moura@hotmail.com>
2023-02-14 15:27:22 -0300

    mapear Dto entidades

---
+++ MegaFilmes.Api/Automapper/FilmeAtorProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using MegaFilmes.Domain.Dtos.FilmeAtorDto;
+using MegaFilmes.Domain.Entities;
+
+namespace MegaFilmes.Api.Automapper;
+
+public class FilmeAtorProfile : Profile
+{
+    public FilmeAtorProfile() 
+    {
+        CreateMap<CreateFilmeAtorDto, FilmeAtor>();
+        CreateMap<UpdateFilmeAtorDto, FilmeAtor>();
+        CreateMap<FilmeAtor, ReadFilmeAtorDto>();
+    }
+}
