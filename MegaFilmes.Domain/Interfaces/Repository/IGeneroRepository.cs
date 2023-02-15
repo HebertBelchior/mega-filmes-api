63e88d62f7cfe9e2e6d3ea176c876403ada7b4d5
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-02-15 12:22:07 -0300

    feat(genero): cria um repositório para o genero

---
+++ MegaFilmes.Domain/Interfaces/Repository/IGeneroRepository.cs
@@ -0,0 +1,9 @@
+﻿using MegaFilmes.Domain.Entities;
+
+namespace MegaFilmes.Domain.Interfaces.Repository;
+
+public interface IGeneroRepository
+{
+    Task<Genero> CreateAync(Genero filme);
+    Task<ICollection<Genero>> GetAllAsync();
+}
