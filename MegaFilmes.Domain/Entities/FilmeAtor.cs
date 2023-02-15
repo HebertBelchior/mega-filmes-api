ff4ea3d0780cfaba031bb1593328ff863bb269d8
Hebert <hebert.moura@hotmail.com>
2023-02-15 02:46:33 -0300

    Adicionar relacionamento entre tabelas no banco de dados

--- MegaFilmes.Domain/Entities/FilmeAtor.cs
+++ MegaFilmes.Domain/Entities/FilmeAtor.cs
@@ -1,11 +1,16 @@
-﻿namespace MegaFilmes.Domain.Entities;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MegaFilmes.Domain.Entities;
 
 public class FilmeAtor
 {
     public int Id { get; set; }
+    public string Papel { get; set; }
+
     public int FilmeId { get; set; }
+    [ForeignKey(nameof(FilmeId))]
     public Filme Filme { get; set; }
     public int AtorId { get; set; }
+    [ForeignKey(nameof(AtorId))]
     public Ator Ator { get; set; }
-    public string Papel { get; set; }
 }
