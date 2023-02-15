f77336c86d5cb1fce8b0199597b002d2f0a62cc2
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-02-15 15:17:50 -0300

    feat(genero): cria o profile para genero e adiciona sevicos no program

---
+++ MegaFilmes.Api/Automapper/GeneroProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using MegaFilmes.Domain.Dtos.GeneroDto;
+using MegaFilmes.Domain.Entities;
+
+namespace MegaFilmes.Api.Automapper;
+
+public class GeneroProfile : Profile
+{
+	public GeneroProfile()
+	{
+		CreateMap<CreateGeneroDto, Genero>();
+		CreateMap<Genero, ReadGeneroDto>();
+    }
+}
