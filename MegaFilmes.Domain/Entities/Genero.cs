a633edd73f05d1e9eb52fb86303f670888656475
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-02-15 12:10:13 -0300

    feat(genero): cria entidade genero e configura no db

---
+++ MegaFilmes.Domain/Entities/Genero.cs
@@ -0,0 +1,8 @@
+﻿namespace MegaFilmes.Domain.Entities;
+
+public class Genero
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public ICollection<Filme> Filme { get; set; }
+}
